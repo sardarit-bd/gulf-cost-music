821af4a567b1259e01f76aadf472a4faf034b3b5
MD <sohelf131@gmail.com>
2025-11-03 15:11:37 +0600

    add: cast changing funcationality

--- src/app/casts/page.jsx
+++ src/app/casts/page.jsx
@@ -1,10 +1,14 @@
+"use client"
+
 import FavoritesList from "@/components/modules/Casts/favorites-list";
 import FeaturedCast from "@/components/modules/Casts/featured-cast";
+import { useState } from "react";
 
 
 export default function CastsSection() {
+  const [cast, setCast] = useState("")
   return (
-    <main
+    <div
       className="py-16 px-6 md:px-16 mt-20"
       style={{
         background: "linear-gradient(to bottom, #F9FAFB 0%, #ffffff 100%)",
@@ -13,14 +17,14 @@ export default function CastsSection() {
       <div className="container mx-auto grid grid-cols-1 lg:grid-cols-3 gap-8">
         {/* Left Column - Favorites */}
         <div className="lg:col-span-1">
-          <FavoritesList />
+          <FavoritesList setCast={setCast} />
         </div>
 
         {/* Right Column - Featured Cast */}
         <div className="lg:col-span-2">
-          <FeaturedCast />
+          <FeaturedCast cast={cast} />
         </div>
       </div>
-    </main>
+    </div>
   )
 }
--- src/app/waves/page.jsx
+++ src/app/waves/page.jsx
@@ -1,9 +1,12 @@
+"use client"
 import FeaturedWave from "@/components/modules/waves/featured-wave";
 import WavesFavoritesList from "@/components/modules/waves/waves-favorites-list";
+import { useState } from "react";
 
 export default function WavesSection() {
+  const [selectedWaves, setSelectedWaves] = useState("")
   return (
-    <main
+    <div
       style={{
         background: "linear-gradient(to bottom, #F9FAFB 0%, #ffffff 100%)",
       }}
@@ -12,14 +15,14 @@ export default function WavesSection() {
       <div className="container mx-auto grid grid-cols-1 lg:grid-cols-3 gap-8">
         {/* Left Column - Favorites */}
         <div className="lg:col-span-1">
-          <WavesFavoritesList />
+          <WavesFavoritesList setWave = {setSelectedWaves} />
         </div>
 
         {/* Right Column - Featured Wave */}
         <div className="lg:col-span-2">
-          <FeaturedWave />
+          <FeaturedWave wave={selectedWaves} />
         </div>
       </div>
-    </main>
+    </div>
   );
 }
