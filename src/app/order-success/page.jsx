4232358257c9269e9a01cabe7407294401823f4f
Ashiqur Rahman <ashiqur.sardarit@gmail.com>
2025-11-13 14:42:50 +0600

    update order-success page

--- src/app/order-success/page.jsx
+++ src/app/order-success/page.jsx
@@ -1,28 +1,10 @@
-"use client";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import OrderSuccessClient from "@/components/modules/merch/order-success/OrderSuccessClient";
+import { Suspense } from "react";
 
-export default function OrderSuccess() {
-    const router = useRouter();
-    const params = useSearchParams();
-    const sessionId = params.get("session_id");
-
-    useEffect(() => {
-        if (sessionId) {
-            setTimeout(() => {
-                router.push("/merch");
-            }, 3000);
-        }
-    }, [sessionId]);
-
+export default function Page() {
     return (
-        <div className="flex flex-col items-center justify-center h-[60vh] text-center">
-            <h1 className="text-3xl font-bold text-green-600">🎉 Payment Successful!</h1>
-            <p className="text-gray-600 mt-2">
-                Thank you for your purchase! Redirecting you to merchandise page...
-            </p>
-
-            <div className="mt-6 animate-bounce text-5xl">✔</div>
-        </div>
+        <Suspense fallback={<div className="p-10 text-center">Loading...</div>}>
+            <OrderSuccessClient />
+        </Suspense>
     );
 }
